b599a969f0d104ad3d469ba20d703ee0776d8dd6
kshubham5464 <kshubham5462@gmail.com>
2025-03-31 16:19:38 +0530

    Updated feature

--- Arrow Function/arrow_function_and_this.js
+++ Arrow Function/arrow_function_and_this.js
@@ -2,13 +2,12 @@
 // Arrow functions do not have their own this, instead, they inherit this from the surrounding scope.
 // �� Key Point: this inside an arrow function refers to the outer function&#39;s this.
 
-
 const person = {
-name: "Shubham",
-greet: function() {
-setTimeout(() => {
-console.log(`Hello, ${this.name}!`);
-}, 1000);
-}
+  name: "Shubham",
+  greet: function () {
+    setTimeout(() => {
+      console.log(`Hello, ${this.name}!`);
+    }, 1000);
+  },
 };
-person.greet(); // Hello, Shubham! after 1 second 
+person.greet(); // Hello, Shubham! after 1 second
